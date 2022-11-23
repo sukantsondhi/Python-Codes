cf29c32771c22108cac61c29c7afdbe7653ee945
Sukant <93847372+sukantsondhi@users.noreply.github.com>
2022-11-23 18:24:30 +0000

    Add files via upload

---
+++ SetSequence.py
@@ -0,0 +1,7 @@
+#For a given array containing integer elements in a set sequence and one element out of sequence, write a program to find the element.
+
+import numpy as np
+
+a = np.array([1,4,4,2,6,3,6,3,7,9,5])
+
+print(set(a))
